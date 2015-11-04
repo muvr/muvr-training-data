1edfc6ee00f355288cd7ca32311977668310cee7
Duc Hoang <duch@cakesolutions.net>
2015-11-04 17:50:01 +0000

    Visualise the whole folder and generate pdf at the same folder

--- tools/view_data.R
+++ tools/view_data.R
@@ -1,14 +1,25 @@
 #!/usr/local/bin/Rscript
 
 args <- commandArgs(trailingOnly = TRUE)
-filename <- args[1]
+folder <- args[1]
 
 # load csv file
-print(paste("loading: ", filename))
-data <- read.csv(filename, header=FALSE)
+print(paste("loading csv in folder:", folder))
 
-# set column names
-names(data) <- c("x", "y", "z", "ex", "int", "kg", "rep")
+list_file <- list.files(path = folder, pattern="*\\.csv$")
 
-# plot x,y,z and ex
-plot.ts(data[c("x", "y", "z", "ex")], main="Sensors data")
+for (i in 1:length(list_file))
+{
+    csv_name <- list_file[i]
+    full_path <- file.path(folder, csv_name)
+    print(paste("analysing this csv:", full_path))
+    data <- read.csv(full_path, header=FALSE)
+    # set column names
+    names(data) <- c("x", "y", "z", "ex", "int", "kg", "rep")
+
+    pdf(paste(full_path, ".pdf", sep = ""))
+
+    # plot x,y,z and ex
+    plot.ts(data[c("x", "y", "z", "ex")], main="Sensors data")
+
+}
